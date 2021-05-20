0584c59eb0ee9628511ad3fc728ab9a798fdf829
Corentin <cgibrat31@gmail.com>
2021-05-20 12:28:54 +0200

    début contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Contact} from '../model/contact.model';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ContactService} from '../services/contact.service';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  contact: Contact;
+  createContactForm: FormGroup;
+  categories: ;
 
-  constructor() { }
+  constructor(private contactService: ContactService,private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initForm();
+    this.contact = new Contact();
   }
 
+  initForm(){
+    this.createContactForm = this.formBuilder.group({
+      message: ['', [Validators.required,Validators.maxLength(130)]],
+      phone: ['', Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")],
+      email: ['', [Validators.email,Validators.required]],
+    });
+  }
+
+  onAddContact() {
+    console.log("onaddcontact")
+  }
 }
